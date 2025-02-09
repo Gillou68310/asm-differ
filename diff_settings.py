a834d8cf17d76df623e3d843a4a11dd74ffefe3e
Gillou68310 <gilles.siberlin@outlook.com>
2025-02-09 19:19:43 +0100

    wip

--- diff_settings.py
+++ diff_settings.py
@@ -4,9 +4,10 @@ def apply(config, args):
     config["mapfile"] = "build.map"
     config["source_directories"] = ["."]
     # config["show_line_numbers_default"] = True
-    # config["arch"] = "mips"
+    config["arch"] = "x86"
     # config["map_format"] = "gnu" # gnu, mw, ms
     # config["build_dir"] = "build/" # only needed for mw and ms map format
-    # config["expected_dir"] = "expected/" # needed for -o
+    config["expected_dir"] = "expected/" # needed for -o
     # config["makeflags"] = []
-    # config["objdump_executable"] = ""
+    config["objdump_executable"] = "./omf-objdump"
+    #config["objdump_flags"] = ["-M","intel"]
